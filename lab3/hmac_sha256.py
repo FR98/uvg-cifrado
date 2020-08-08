e47f01468653e37ea93de9a8b0f88b35bd96f1a4
FR98 <ros18676@uvg.edu.gt>
2020-08-08 11:41:23 -0600

    Fix HMAC SHA256 implementation

--- lab3/hmac_sha256.py
+++ lab3/hmac_sha256.py
@@ -11,10 +11,21 @@ def hmacSha256(msg, key):
     # message = bytes(msg, 'utf-8')
     # secret = bytes(key, 'utf-8')
     
-    return b64encode(hmac.new(secret, message, digestmod=hashlib.sha256).digest())
+    print("Message:\t{message}\nKey:\t\t{key}".format(message=message, key=key))
+    print("HMAC:\t\t{}".format(hmac.new(secret, message, digestmod=hashlib.sha256).hexdigest()))
+    print("HMAC encoded:\t{}".format(b64encode(hmac.new(secret, message, digestmod=hashlib.sha256).digest())))
+    print("\n")
 
-print(hmacSha256('Cifrado de informacion seccion 10', 'CC3078'))
-print(hmacSha256('La implementacion de este ejercicio fue sencilla', 'MAC'))
+hmacSha256('Cifrado de informacion seccion 10', 'CC3078')
+# >> gGxuGFVOcKSrisiMn2RFZvrukuCvALr2Pl4pM/psqc0=
+# Encoded
+# >> 806c6e18554e70a4ab8ac88c9f644566faee92e0af00baf63e5e2933fa6ca9cd
+
+hmacSha256('La implementacion de este ejercicio fue sencilla', 'MAC')
+# >> Zlc36/M+z15ItsFPusomEsC/m/k9JAv9Lauf5mmQBUE=
+# Encoded
+# >> 665737ebf33ecf5e48b6c14fbaca2612c0bf9bf93d240bfd2dab9fe669900541
+
 
 
 # Implementacion del timing attack extraida de
