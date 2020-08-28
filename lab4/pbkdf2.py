df592e414e44ca5ced697caceff230b704497170
FR98 <ros18676@uvg.edu.gt>
2020-08-27 19:21:48 -0600

    Un input

--- lab4/pbkdf2.py
+++ lab4/pbkdf2.py
@@ -2,7 +2,7 @@ import os, binascii
 from backports.pbkdf2 import pbkdf2_hmac
 
 salt = binascii.unhexlify('aaef2d3f4d77ac66e9c5a6c3d8f921d1')
-password = "mi contrase;a".encode("utf8")
+password = input("Ingresa password: ").encode("utf8")
 key = pbkdf2_hmac("sha256", password, salt, 50000, 32)
 
 print("Derived key:", binascii.hexlify(key))
