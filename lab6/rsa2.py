67ee126d007ed7f1ff5f5b2b5cb0a6e2145fd2cf
FR98 <ros18676@uvg.edu.gt>
2020-09-26 15:28:22 -0600

    Add rsa library

--- lab6/rsa2.py
+++ lab6/rsa2.py
@@ -1,7 +1,12 @@
 import rsa
 
-(publicKey, privateKey) = rsa.newkeys(128)
+(publicKey, privateKey) = rsa.newkeys(512)
 
-print(publicKey)
-print()
-print(privateKey)
+message = input("Ingrese un mensaje: ")
+message = message.encode('utf-8')
+
+cryto_message = rsa.encrypt(message, publicKey)
+print(cryto_message)
+
+decrypted_message = rsa.decrypt(cryto_message, privateKey)
+print(decrypted_message.decode('utf-8'))
