6997c0a234f185c18894f6c7ab3cfe3d465a563e
Marco <marcofuentes1607@gmail.com>
2020-10-01 19:13:34 -0600

    Comentarios y otros eliminados

--- lab7-elgamal/elgamalAlgorithm.py
+++ lab7-elgamal/elgamalAlgorithm.py
@@ -1,47 +1,5 @@
 from random import randint
 
-"""
-Proceso:
-- Se fija un numero primo p largo (600 digitos o 2000 bits)
-- Se fija un entero g que esta entre {1, ...., p}
-- Alice elige aleatoriamente un numero a entre {1, ...., p-1}
-- Alice calcula A = g^a (mod p) y se lo envia a Bob
-- Bob calcula B = g^b (mod p) y se lo envia a Alice
-- * Entonces se creo la llave Kab = g^ab (mod p)
-- Alice y Bob pueden calcular Kab
-    - Alice obtiene Kab asi:
-        Kab = B^a (mod p) = (g^b)^a
-    - Bob obtiene Kab asi:
-        Kab = A^b (mod p) = (g^a)^b
-
-Importante:
-    - p y g son publicos
-    - a y b no son publicos
-
-Los atacantes ven los valores de:
-    p, g, A, B
-
-Este protocolo es indefenso a man-in-the-middle, si un atacante intercepta A y B y los reemplaza por A' y B', este atacante controla la comunicacion
-"""
-
-# # Variables iniciales
-# primo = 23
-# generador = randint(1, primo)
-
-# a, b = randint(1, primo-1), randint(1, primo-1)
-
-# A = (generador ** a) % primo
-# B = (generador ** b) % primo
-
-# aliceSecreto = (B ** a) % primo
-# bobSecreto = (A ** b) % primo
-
-
-
-
-
-#Python program to illustrate ElGamal encryption 
-  
 import random  
 from math import pow
   
