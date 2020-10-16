9e432de1ec5dc062736fee03687dd14d029b5558
Marco <marcofuentes1607@gmail.com>
2020-10-15 18:38:51 -0600

    Implementacion de Firma digital

---
+++ lab8-signature/Signature.py
@@ -0,0 +1,38 @@
+"""
+Signature.py
+Implementacion de firima digital usando el algoritmo de Elgamal
+"""
+from random import randint
+import random
+from math import pow
+from sha256 import hmacSha256
+from elGamal import encrypt, decrypt, randomPrime , gen_key
+
+min = pow(10, 5)
+max = pow(10, 6)
+
+MESSAGE = "Este es un mensaje de prueba"
+
+primo = randomPrime(min, max)
+print('Primo: ', primo)
+g = random.randint(2, primo)
+print('Generador: ', g)
+
+
+# Info de Alice
+aSecret = gen_key(primo)
+aPublic = (g ** aSecret) % primo
+
+# Enviar informacion a Bob
+
+myHash = hmacSha256( msg = MESSAGE , key = str(aPublic) )
+encrypted , bPublic = encrypt(msg = myHash ,primo = primo , A = aPublic , g = g)
+
+# Verificacion de Alice
+
+alicesHash = hmacSha256(msg = MESSAGE , key = str(aPublic) )
+decrypted = decrypt(encrypted , bPublic , aSecret , primo)
+decrypted=''.join(decrypted)
+print('*'*50)
+print('FUNCIONO: ' , decrypted== alicesHash)
+print('*'*50)
---
+++ lab8-signature/sha256.py
@@ -0,0 +1,39 @@
+"""
+sha256.py
+"""
+
+import hashlib
+import hmac
+from base64 import b64encode
+
+
+def hmacSha256(msg, key):
+    message = bytes(source = msg, encoding = 'utf-8')
+    secret  = bytes(source = key, encoding = 'utf-8')
+    return hmac.new(secret, message, digestmod=hashlib.sha256).hexdigest()
+    # print("Message:\t{message}\nKey:\t\t{key}".format(
+    #     message=message, key=key))
+    # print("HMAC:\t\t{}".format(hmac.new(secret, message, digestmod=hashlib.sha256).hexdigest()))
+    # print("HMAC encoded:\t{}".format(
+    #     b64encode(hmac.new(secret, message, digestmod=hashlib.sha256).digest())))
+    # print("\n")
+
+
+# hmacSha256('Cifrado de informacion seccion 10', 'CC3078')
+# >> gGxuGFVOcKSrisiMn2RFZvrukuCvALr2Pl4pM/psqc0=
+# Encoded
+# >> 806c6e18554e70a4ab8ac88c9f644566faee92e0af00baf63e5e2933fa6ca9cd
+
+# hmacSha256('La implementacion de este ejercicio fue sencilla', 'MAC')
+# >> Zlc36/M+z15ItsFPusomEsC/m/k9JAv9Lauf5mmQBUE=
+# Encoded
+# >> 665737ebf33ecf5e48b6c14fbaca2612c0bf9bf93d240bfd2dab9fe669900541
+
+
+# Implementacion del timing attack extraida de
+# https://github.com/sqreen/DevelopersSecurityBestPractices/tree/master/_practices/timing-attack/_python
+
+# To run
+# pip install -r requirements.txt
+# gunicorn -w 1 app:app
+# python hack.py
